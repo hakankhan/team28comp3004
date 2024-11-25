c7e245e21cda6943f33f4039d765b927eccab6c3
GrangerB03 <bengranger@live.ca>
2024-11-25 12:27:23 -0500

    help me please please please please

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -9,16 +9,12 @@ MainWindow::MainWindow(QWidget *parent)
     ui->main_stack->setCurrentIndex(0);
     ui->results_tabs->setCurrentIndex(0);
     c = new Control();
-<<<<<<< HEAD
-    connect(ui->submit_button, SIGNAL(clicked()), this, SLOT(on_submit_button_clicked()));
-=======
     for(int i = 0; i < MAX_PROFILES; i++){
         index_to_profile[i] = NULL;
     }
     connect(ui->submit_button, SIGNAL(clicked()), this, SLOT(submit_button_clicked()));
     connect(ui->switch_profile_box, SIGNAL(activated(int)), this, SLOT(profile_changed()));
     connect(ui->new_profile_button, SIGNAL(clicked()), this, SLOT(new_profile_button_clicked()));
->>>>>>> 9803a8765763de9e3eb587bf485d985f56f57bc7
 }
 
 MainWindow::~MainWindow()
@@ -26,9 +22,6 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-<<<<<<< HEAD
-void MainWindow::on_submit_button_clicked()
-=======
 void MainWindow::profile_changed(){
     qInfo("changed");
     qInfo("old profile id: %d", c->get_current_profile()->get_id());
@@ -37,7 +30,6 @@ void MainWindow::profile_changed(){
 }
 
 void MainWindow::submit_button_clicked()
->>>>>>> 9803a8765763de9e3eb587bf485d985f56f57bc7
 {
     // push profile to control
     QString fname = ui->first_name->text();
@@ -55,19 +47,10 @@ void MainWindow::submit_button_clicked()
     int num_list_elements = -1;
     for(int i = 0; i < MAX_PROFILES; i++){
         Profile* p = c->get_profiles()[i];
-<<<<<<< HEAD
-        qInfo("Profiles array element:");
-        qInfo("i = %d", i);
-        if(p != NULL){
-            qInfo("Not null!");
-            index_to_profile[num_list_elements++] = p;
-            ui->switch_profile_box->addItem("Profile " + QString(num_list_elements) + ": " + p->get_first_name());
-=======
         num_list_elements++;
         if(p != NULL){
             index_to_profile[num_list_elements] = p;
             ui->switch_profile_box->addItem("Profile " + QString::number(num_list_elements) + ": " + p->get_first_name());
->>>>>>> 9803a8765763de9e3eb587bf485d985f56f57bc7
             continue;
         }
 
